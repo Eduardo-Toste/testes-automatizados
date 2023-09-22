07f32fff2d68d1327d2297d17fb93ff8863b1b3c
Eduardo Toste <duciochetti@gmail.com>
2023-09-22 17:22:29 -0300

    Testes JUnit

---
+++ calculadoraBasica/src/Calculadora/TestesJUnit.java
@@ -0,0 +1,14 @@
+package Calculadora;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestesJUnit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
